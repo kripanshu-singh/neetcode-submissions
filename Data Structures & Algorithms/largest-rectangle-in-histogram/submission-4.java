89ec2b2da2171e61d9df46db70bcd157233e7ab8
Kripanshu SIngh <143623713+kripanshu-singh@users.noreply.github.com>
2026-05-26 13:47:13 +0530

    Add: largest-rectangle-in-histogram - submission-4

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-4.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> nse = new ArrayDeque<>();
+        Deque<Integer> pse = new ArrayDeque<>();
+        int n = heights.length;
+        if (n == 1)
+            return heights[0];
+        int[] nseArr = new int[n];
+        int[] pseArr = new int[n];
+        Arrays.fill(nseArr, n);
+        Arrays.fill(pseArr, -1);
+        for (int i = 0; i < n; i++) {
+            while (!nse.isEmpty() && heights[nse.peek()] > heights[i]) {
+                int ind = nse.pop();
+                nseArr[ind] = i;
+            }
+            nse.push(i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!pse.isEmpty() && heights[pse.peek()] > heights[i]) {
+                int ind = pse.pop();
+                pseArr[ind] = i;
+            }
+            pse.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // System.out.print("NSE: " + nseArr[i]);
+            // System.out.print(" PSE: " + pseArr[i]);
+            // System.out.print("\n");
+            int length = heights[i];
+            int breadth = nseArr[i] - pseArr[i] - 1;
+            int area = length * breadth;
+            ans = Math.max(ans, area);
+        }
+        return ans;
+    }
+}
