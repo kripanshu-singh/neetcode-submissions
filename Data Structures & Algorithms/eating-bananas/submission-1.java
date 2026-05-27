700d437b4c9705305f34602dc6c90100169dff36
Kripanshu SIngh <143623713+kripanshu-singh@users.noreply.github.com>
2026-05-27 13:23:29 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean check_K(int k, int[] piles, int h) {
+        int hourToEat = 0;
+        for (int i = piles.length - 1; i >= 0; i--) {
+            hourToEat += (piles[i] + k - 1) / k;
+            // System.out.println("K: " + k + " hourToEat: " + hourToEat);
+            if (hourToEat > h)
+                return false;
+        }
+        return true;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int n = piles.length;
+        int start = 1;
+        int end = piles[n - 1];
+        int ans = 0;
+        while (start <= end) {
+            int k = start + (end - start) / 2;
+            // System.out.println("START: "+start+" END: "+end+" MID: "+k);
+            if (check_K(k, piles, h)) {
+                ans = k;
+                end = k-1;
+            } else {
+                start = k + 1;
+            }
+        }
+        return ans;
+    }
+}
