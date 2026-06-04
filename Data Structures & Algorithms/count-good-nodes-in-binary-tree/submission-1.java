102b33c470f8bc6db144d78ebee207d8ae0f4e72
Kripanshu SIngh <143623713+kripanshu-singh@users.noreply.github.com>
2026-06-04 10:36:47 +0530

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solve(TreeNode root, int maxVal) {
+        if (root == null)
+            return 0;
+        maxVal = Math.max(maxVal, root.val);
+        int leftAns = solve(root.left, maxVal);
+        int rightAns = solve(root.right, maxVal);
+
+        if (root.val == maxVal)
+            return leftAns + rightAns + 1;
+
+        return leftAns + rightAns;
+    }
+    public int goodNodes(TreeNode root) {
+        return solve(root, root.val);
+    }
+}
